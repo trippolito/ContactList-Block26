70c012f4abeb130796ec757220e7b12ce76c10ce
Travyynn Ippolito <ippolito3717@gmail.com>
2023-11-15 17:09:59 -0500

    got full contact list to dislay on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import ContactList from './components/ContactList';
+// import ContactRow from './components/ContactRow';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+      <ContactList />
 
     </>
   )
---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,14 @@
+
+const ContactRow = ({ contact }) => {
+
+
+  return (
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  );
+}
+
+export default ContactRow;
